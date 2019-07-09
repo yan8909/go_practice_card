93ba41ffd93079fdd9831d7ef73b224fec2b519e
yan8909 <43824130+yan8909@users.noreply.github.com>
2019-07-09 13:56:47 +0800

    functions and return

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
