a22675ab0209a7b0dfbf58ac70d07cfc8e744872
yan8909 <43824130+yan8909@users.noreply.github.com>
2019-07-10 13:22:28 +0800

    custom type

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	//var card string = "Ace of Spades"
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
+	cards.print()
 }
 
 func newCard() string {
